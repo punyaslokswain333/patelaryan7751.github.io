09267463802dc2b488fca766f99df1fd50c5df90
patelaryan7751 <59426397+patelaryan7751@users.noreply.github.com>
2020-05-23 08:42:51 +0530

    Update app.js

--- ideainnovationcellwebsite/app.js
+++ ideainnovationcellwebsite/app.js
@@ -1,6 +1,6 @@
 var defferedPrompt;
 if ('serviceWorker' in navigator){
-    navigator.serviceWorker.register('/sw.js').then(function(){
+    navigator.serviceWorker.register('../sw.js').then(function(){
 console.log("sw registered");
     });
 }
