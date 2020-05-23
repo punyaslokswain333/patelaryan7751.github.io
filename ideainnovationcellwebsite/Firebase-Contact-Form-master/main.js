c569f89f9325fc0d677607bf6f511ad23d9225ed
patelaryan7751 <59426397+patelaryan7751@users.noreply.github.com>
2020-05-23 15:37:35 +0530

    update

--- ideainnovationcellwebsite/Firebase-Contact-Form-master/main.js
+++ ideainnovationcellwebsite/Firebase-Contact-Form-master/main.js
@@ -14,18 +14,33 @@ firebase.initializeApp(config);
 const messaging = firebase.messaging();
 messaging.requestPermission().then(function(){
     console.log("granted");
-    return messaging.getToken();
-})
- .then(function(token){
-    console.log(token);
-})
-    .catch(function(err){
+   getRegisterToken();
+}).catch(function(err){
     console.log("user denied");
 });
+function getRegisterToken(){
+    
 
-messaging.onMessage(function(payload){
-    console.log('onMessage: ',payload);
+// Get Instance ID token. Initially this makes a network call, once retrieved
+// subsequent calls to getToken will return from cache.
+messaging.getToken().then((currentToken) => {
+  if (currentToken) {
+      console.log(currentToken);
+    sendTokenToServer(currentToken);
+    updateUIForPushEnabled(currentToken);
+  } else {
+    // Show permission request.
+    console.log('No Instance ID token available. Request permission to generate one.');
+    // Show permission UI.
+    updateUIForPushPermissionRequired();
+    setTokenSentToServer(false);
+  }
+}).catch((err) => {
+  console.log('An error occurred while retrieving token. ', err);
+  showToken('Error retrieving Instance ID token. ', err);
+  setTokenSentToServer(false);
 });
+}
 
 
 
