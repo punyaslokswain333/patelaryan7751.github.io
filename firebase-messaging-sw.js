643ef474d60323ea978165a144c155a9d14cb862
patelaryan7751 <59426397+patelaryan7751@users.noreply.github.com>
2020-05-24 14:03:53 +0530

    Revert "Update firebase-messaging-sw.js"
    This reverts commit 156be1816a7ae28bc1f1f2369e08a982045995c4.

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -18,7 +18,7 @@ messaging.setBackgroundMessageHandler(function(payload){
     const title='Hello World';
     const options={
       body:payload.data.status,
-      icon:'/icl3.png'
+      image:icl3.png,
     };
     return self.registration.showNotification(title,options);
     
