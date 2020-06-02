69a12736eb243ab1d825673135926f3a51189716
patelaryan7751 <59426397+patelaryan7751@users.noreply.github.com>
2020-06-02 09:51:04 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -17,7 +17,7 @@ const messaging= firebase.messaging();
 messaging.setBackgroundMessageHandler(function(payload){
     const title='Hello World';
     const options={
-      body:payload.data.status,
+      body:payload.data.message,
       image:icl3.png,
     };
     return self.registration.showNotification(title,options);
