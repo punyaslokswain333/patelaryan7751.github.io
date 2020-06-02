f9cc65ec2e921d5a1944764c083630697b24dde4
patelaryan7751 <59426397+patelaryan7751@users.noreply.github.com>
2020-06-02 06:42:35 +0530

    UPDATE

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,14 +2,14 @@ importScripts('https://www.gstatic.com/firebasejs/4.3.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/4.3.0/firebase-messaging.js');
 
 var config = {
-apiKey: "AIzaSyCFXQqvJGGpcNeSittYro6zy5e03itmDLg",
-  authDomain: "ideainnovationcell.firebaseapp.com",
-  databaseURL: "https://ideainnovationcell.firebaseio.com",
-  projectId: "ideainnovationcell",
-  storageBucket: "ideainnovationcell.appspot.com",
-  messagingSenderId: "368936158980",
-  appId: "1:368936158980:web:14751bb19594425bfcb251",
-  measurementId: "G-4E9R4VDDRL"
+apiKey: "AIzaSyBC68Ro-7v9iadpGNIRU2ZKgbGL4qJyspA",
+    authDomain: "hospicare-f1c30.firebaseapp.com",
+    databaseURL: "https://hospicare-f1c30.firebaseio.com",
+    projectId: "hospicare-f1c30",
+    storageBucket: "hospicare-f1c30.appspot.com",
+    messagingSenderId: "467763068387",
+    appId: "1:467763068387:web:5b9eb2a0141cc2125434d6",
+    measurementId: "G-YS3HN5H0J1"
 };
 firebase.initializeApp(config);
 
